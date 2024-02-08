2f0b7a44eee088c02abe2647ce3e81038a7032f4
Stephen Lane <stephen.lane@cso.ie>
2024-02-08 11:24:54 +0000

    .net8 initial commit

---
+++ src/CSO.Sanitizer/Middleware/SanitizerServicesHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CSO.Sanitizer
+{
+    public class SanitizerServicesHelper
+    {
+
+        public static IConfiguration Configuration { get; set; }
+        public static ServiceProvider ServiceProvider { get; set; }
+
+
+        public static ISanitizer Sanitizer;
+
+        public static ICleanser Cleanser;
+
+    }
+}
---
+++ src/CSO.Sanitizer/Middleware/ServiceConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CSO.Sanitizer
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddSanitizer(this IServiceCollection service, WebApplicationBuilder builder, dynamic LogObject)
+        {
+
+            service.AddSingleton<ICleanser, Cleanser>();
+            service.AddSingleton<ISanitizer, Sanitizer>();
+
+            var sp = service.BuildServiceProvider();
+
+            SanitizerServicesHelper.ServiceProvider = sp;
+            SanitizerServicesHelper.Configuration = builder.Configuration;
+
+            SanitizerServicesHelper.Sanitizer = sp.GetRequiredService<ISanitizer>();
+            SanitizerServicesHelper.Cleanser = sp.GetRequiredService<ICleanser>();
+
+
+            LogObject.Info("Sanitizer setup completed");
+            return service;
+        }
+
+    }
+}
